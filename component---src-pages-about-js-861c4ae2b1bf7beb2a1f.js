0aa8495579ec4fa1c125ba697e5b5d4bc153018b
Arden Rasmussen <ardenisthebest@gmail.com>
2026-05-11 11:20:46 -0700

    2026-05-11 11:20:46

--- component---src-pages-about-js-861c4ae2b1bf7beb2a1f.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"3XHS":function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a);n.default=function(){return l.a.createElement("div",null,l.a.createElement("h1",null,"Hello"),l.a.createElement("p",null,"This is the about page"))}}}]);
-//# sourceMappingURL=component---src-pages-about-js-861c4ae2b1bf7beb2a1f.js.map
